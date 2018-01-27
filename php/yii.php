65c6a95fd94da34d24cbc0112b14a9499bcfb4aa
lizengcai <lizengcai@appublisher.com>
2018-01-27 20:06:28 +0800

    version 3.0.0

--- php/yii.php
+++ php/yii.php
@@ -28,3 +28,60 @@ $arr = [
         'bootstrap.php' => 'Yii::setAlias(\'test\', FILEPATH)'
     ]
 ];
+
+/**
+ * 4.function
+ */
+$this->goHome(); //回到首页
+$this->goBack(); //回到上一级
+$this->redirect(['site/index']); //重定向
+$model->findOne($id = 12); //查找一条数据
+
+/**
+ * 5.request property and method
+ */
+\Yii::$app->request->isAjax;
+\Yii::$app->request->isPost;
+\Yii::$app->request->userAgent;
+\Yii::$app->request->userIP;
+\Yii::$app->request->get(); //获取get全部数据
+\Yii::$app->request->get('username'); //读取get数据
+
+
+
+/**
+ * 6.model
+ */
+$arr = [
+    'extend' => '\'yii\db\ActiveRecord\', \'yii\base\Model\''
+];
+
+/**
+ * 7.table form
+ */
+use yii\helpers\Html;
+Html::beginForm(
+    '', //提交到的方法
+    'post', //传递方式
+    [
+        'id' => 'addFrom',
+        'class' => 'form',
+        'data' => 'fm'
+    ] //define class and id,(options)
+); //begin form
+Html::endForm(); //end form
+Html::input(
+    'text', //type:text/password/hidden
+    'name', //name value
+    'sister', //default value
+    ['class' => 'input password'] //options
+); //input label
+Html::passwordInput( //类似的还有textInput/hiddenInput
+    'pwd', //name
+    '', //default value
+    ['id' => 'password'] //options
+);
+
+/**
+ * 8.
+ */
