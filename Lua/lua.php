6629873fa0e3712267b5b713b703ff53cef061f2
lizengcai <lizengcai_vip@sina.com>
2018-02-22 15:20:37 +0800

    h

--- Lua/lua.php
+++ Lua/lua.php
@@ -7,6 +7,6 @@ curl -R -O http://www.lua.org/ftp/lua-5.3.0.tar.gz
 tar zxf lua-5.3.0.tar.gz
 cd lua-5.3.0
 yum install libtermcap-devel ncurses-devel libevent-devel readline-devel(linux环境安装)
-make linux test(linux)/make macosx test(mac)
+make linux test(linux)make macosx test(mac)
 make install
 '];
