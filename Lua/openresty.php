3c1ebe116fa93611ead60b3dc8681d7fbfc5affc
lizengcai <lizengcai_vip@sina.com>
2018-03-07 17:15:05 +0800

    php模拟put/delete发送/接收/处理,nginx+lua

--- Lua/openresty.php
+++ Lua/openresty.php
@@ -51,3 +51,74 @@ ngx.exit(http-status), //结束请求并输出状态码,ngx.HTTP_SERVICE_UNAVAIL
 ngx.flush(), //将缓冲区内容输出至页面
 
 '];
+
+/**
+ * 6.nginx server配置
+ */
+$arr = ["
+lua_code_cache off; //lua代码缓存,这里off为了避免每次修改重新reload的麻烦,生产环境为on
+lua_package_path '$prefix/lua/?.lua;/blah/?.lua;;'; //$prefix:指nginx的安装目录,两个;;指默认寻址目录(用于require寻址)
+content_by_lua_file content_by_lua_file /Users/cctv/Documents/project/cctvnews_api/newscctv/$path.lua; //逻辑处理
+access_by_lua_file /Users/cctv/Documents/project/cctvnews_api/newscctv/api_2_5_0/common/common.lua; //验证文件,做ip限制/格式转化等
+"];
+
+/**
+ * 7.内置绑定变量访问
+ */
+$arr = [' //地址:http://wiki.jikexueyuan.com/project/openresty/openresty/inline_var.html
+ngx.var.remote_addr //客户端ip
+ngx.var.server_addr //服务器ip
+ngx.var.nginx_version //nginx版本号
+
+//例子(限制ip):
+local black_ips = {["127.0.0.1"] = true}
+local ip = ngx.var.remote_addr
+if true == black_ips[ip] then
+    ngx.exit(ngx.HTTP_FORBIDDEN)
+end
+'];
+
+/**
+ * 8.限制网速
+ */
+$arr = ['
+location /download {
+            access_by_lua_block {
+                ngx.var.limit_rate = 1000
+            };
+        }
+'];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- php/php.php
+++ php/php.php
@@ -10,5 +10,14 @@ mb_substr('中文', 0, 1, 'utf-8');
 "];
 
 /**
- * 2.
+ * 2.curl模拟put/delete发送,并用php接收,jquery也可实现put/delete发送(更改method参数)
  */
+$arr = ["
+curl -v -X PUT http://composer.com/test.php -d 'a=b&c=d' //-v:显示请求信息 -X:指定发送方式 -d:参数
+$_SERVER\['REQUEST_METHOD'] //接收请求方式,如put/delete/post
+file_get_contents('php://input') //接收put参数
+curl -v -X DELETE http://composer.com/test.php?a=b
+$_SERVER\['QUERY_STRING'] //接收delete数据
+
+parse_str($str_param, $array) //解析参数字符串为数组,并赋值给$array
+"];
