0a8e453d7219b2e58603c070673579764cab3081
lizengcai <lizengcai_vip@sina.com>
2018-03-26 17:24:23 +0800

    添加cookie加密/客户端ip获取/redis操作

--- php/function.php
+++ php/function.php
@@ -22,3 +22,84 @@ private function getIP() {
     }
     return $cip;
 }
+//获取客户端ip
+public function ip() {
+    $ip = '';
+    //strcasecmp 比较两个字符，不区分大小写。返回0，>0，<0。
+    if(getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), 'unknown')) {
+        $ip = getenv('HTTP_CLIENT_IP');
+    } elseif(getenv('HTTP_X_FORWARDED_FOR') && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), 'unknown')) {
+        $ip = getenv('HTTP_X_FORWARDED_FOR');
+    } elseif(getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), 'unknown')) {
+        $ip = getenv('REMOTE_ADDR');
+    } elseif(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], 'unknown')) {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return($ip);
+}
+
+/**  摘自 discuz
+ * $string 明文或密文
+ * $operation 加密ENCODE或解密DECODE
+ * $key 密钥
+ * $expiry 密钥有效期 ， 默认是一直有效
+ */
+//加密解密函数(可用来加密cookie)
+if(!function_exists("auth_code")){
+    function auth_code($string, $operation = 'DECODE', $key = '', $expiry = 0) {
+        /*
+            动态密匙长度，相同的明文会生成不同密文就是依靠动态密匙
+            加入随机密钥，可以令密文无任何规律，即便是原文和密钥完全相同，加密结果也会每次不同，增大破解难度。
+            取值越大，密文变动规律越大，密文变化 = 16 的 $ckey_length 次方
+            当此值为 0 时，则不产生随机密钥
+        */
+        $ckey_length = 4;
+        $key = md5($key != '' ? $key : "fdsfdf43535svxfsdfdsfs"); // 此处的key可以自己进行定义，写到配置文件也可以
+        $keya = md5(substr($key, 0, 16));
+
+        $keyb = md5(substr($key, 16, 16));
+        $keyc = $ckey_length ? ($operation == 'DECODE' ? substr($string, 0, $ckey_length): substr(md5(microtime()), -$ckey_length)) : '';
+        $cryptkey = $keya.md5($keya.$keyc);
+        $key_length = strlen($cryptkey);
+        // 明文，前10位用来保存时间戳，解密时验证数据有效性，10到26位用来保存$keyb(密匙b)，解密时会通过这个密匙验证数据完整性
+        // 如果是解码的话，会从第$ckey_length位开始，因为密文前$ckey_length位保存 动态密匙，以保证解密正确
+        $string = $operation == 'DECODE' ? base64_decode(substr($string, $ckey_length)) : sprintf('%010d', $expiry ? $expiry + time() : 0).substr(md5($string.$keyb), 0, 16).$string;
+        $string_length = strlen($string);
+
+        $result = '';
+        $box = range(0, 255);
+
+        $rndkey = array();
+        for($i = 0; $i <= 255; $i++) {
+            $rndkey[$i] = ord($cryptkey[$i % $key_length]);
+        }
+
+        for($j = $i = 0; $i < 256; $i++) {
+            $j = ($j + $box[$i] + $rndkey[$i]) % 256;
+            $tmp = $box[$i];
+            $box[$i] = $box[$j];
+            $box[$j] = $tmp;
+        }
+
+        for($a = $j = $i = 0; $i < $string_length; $i++) {
+            $a = ($a + 1) % 256;
+            $j = ($j + $box[$a]) % 256;
+            $tmp = $box[$a];
+            $box[$a] = $box[$j];
+            $box[$j] = $tmp;
+            $result .= chr(ord($string[$i]) ^ ($box[($box[$a] + $box[$j]) % 256]));
+        }
+
+        if($operation == 'DECODE') {
+            if((substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0) && substr($result, 10, 16) == substr(md5(substr($result, 26).$keyb), 0, 16)) {
+                return substr($result, 26);
+            } else {
+                return '';
+            }
+        } else {
+            // 把动态密匙保存在密文里，这也是为什么同样的明文，生产不同密文后能解密的原因
+            // 因为加密后的密文可能是一些特殊字符，复制过程可能会丢失，所以用base64编码
+            return $keyc.str_replace('=', '', base64_encode($result));
+        }
+    }
+}
--- php/redis.php
+++ php/redis.php
@@ -8,6 +8,109 @@ $arr = [
     'pconnect' => '脚本执行完资源不立即释放,而是放在php-fpm进程中',
 ];
 
+/**
+ * 2.ordinary command
+ */
+$arr = <<<Command
+exists key,
+del key,
+type key, //返回key的类型
+randomkey, //从当前数据库返回随机key
+rename oldkey newkey, //给key重命名(原子)
+
+expire key seconds, //给key设定超时时间(秒)
+ttl key, //查看剩余过期时间
+persist key, //消除超时
+
+setex key time value, //给key设置值和有效时长
+mset key value [key1 value1], //给多个key设值
+msetnx key value [key1 value1], //判断key是否存在并设值
+mget key [key1], //获取多个key值
+incr/decr key, //给键加一或减一,如果key不存在,则给key设为1/-1,
+incrby/decrby key integer, //增加或减少指定值
+incrbyfloat key floatnum, //增加浮点数,注:没有decrbyfloat
+append key value, //追加字符串
+strlen key, //返回key的长度
+
+redis-cli --eval myscript.lua key1 key2,arg1 arg2 arg3 //执行lua脚本
+Command;
+
+/**
+ * 3.set command
+ * 无序，元素唯一不可重复，可用于去重
+ */
+$arr = <<<Set
+smembers key, //返回集合中的所有成员
+sadd key [member1] [member2], //集合中添加元素
+scard key, //返回集合的成员数
+srem key [member1] [member2], //集合中去除成员
+substr key start end, //截取字符串,不改变字符串的值,下标从0开始
+setrange key offset value, //改写字符串
+getrange key start end, //getrange和substr功能类似
+
+Set;
+
+/**
+ * 4.list command
+ * 有序，元素可重复，可用作队列
+ */
+$arr = <<<List
+lpush key string [string2], //江湖规矩:lpush,rpop
+lpushx key string1 [string2], //该key存在时,才向key中插入值
+linsert key before/after cc ccc, //在cc前/后插入ccc
+llen key, //查看列表长度
+lindex key index_value, //查看index对应的值
+lrange key start end, //查看一段列表,lrange key 0 -1:查看全部数据
+ltrim key start end, //截取元素(key值变为截取后的值)
+
+lrem key count value, //count正负代表从左右去除值为value的count个元素
+lpop/rpop,
+
+lset key index value, //设定key[index]的值为value,index必须存在
+blpop key1 [key2] timeout, //timeout设为0时,如果key1,key2不存在或者为空则一直处于堵塞状态,详情看第五点
+List;
+
+/**
+ * 5.堵塞队列详解
+ */
+$arr = <<<Blocking
+“blpop key1...keyN timeout 
+从左到右扫描返回对第一个非空list进行lpop操作并返回，比如blpop list1 list2 list3 0 ,
+如果list不存在list2,list3都是非空则对list2做lpop并返回从list2中删除的元素。如果所有的list都是空或不存在，
+则会阻塞timeout秒，timeout为0表示一直阻塞。当阻塞时，如果有client对key1...keyN中的任意key进行push操作，
+则第一在这个key上被阻塞的client会立即返回（返回键和值）。如果超时发生，则返回nil。有点像unix的select或者poll。
+
+brpop
+同blpop，一个是从头部删除一个是从尾部删除。
+
+注意：不要采用其作为ajax的服务端推送，因为连接有限，遇到问题连接直接打满。
+
+“BLPOP/BRPOP 的先到先服务原则 如果有多个客户端同时因为某个列表而被阻塞，那么当有新值被推入到这个列表时，
+服务器会按照先到先服务（first in first service）原则，优先向最早被阻塞的客户端返回新值。举个例子，
+假设列表 lst 为空，那么当客户端 X 执行命令 BLPOP lst timeout 时，客户端 X 将被阻塞。在此之后，
+客户端 Y 也执行命令 BLPOP lst timeout ，也因此被阻塞。如果这时，客户端 Z 执行命令 RPUSH lst "hello" ，
+将值 "hello" 推入列表 lst ，那么这个 "hello" 将被返回给客户端 X ，而不是客户端 Y ，因为客户端 X 的被阻塞
+时间要早于客户端 Y 的被阻塞时间。”
+
+“rpoplpush/brpoplpush：rpoplpush srckey destkey 从srckey对应list的尾部移除元素并添加到destkey对应
+list的头部,最后返回被移除的元素值，整个操作是原子的.如果srckey是空或者不存在返回nil，注意这是唯一一个操作
+两个列表的操作，用于两个队列交换消息。
+
+应用场景：task + bak 双链表完成工作任务转交的安全队列，保证原子性。 
+业务逻辑: 1: Rpoplpush task bak 2: 接收返回值,并做业务处理 3: 完成时用LREM消掉。
+如不成功或者如果集群管理(如zookeeper)发现worker已经挂掉,下次从bak表里取任务”
+
+“另一个应用场景是循环链表： 127.0.0.1:6379> lrange list 0 -1 
+1) "c" 2) "b" 3) "a" 
+127.0.0.1:6379> rpoplpush list list "a" 
+127.0.0.1:6379> lrange list 0 -1 1) "a" 2) "c" 3) "b”
+
+摘录来自: Pengcheng Huang. “Redis开发运维实践指南”。 iBooks. 
+Blocking;
+
+
+
+
 
 
 
