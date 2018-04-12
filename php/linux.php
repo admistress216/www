969f3dbebfdeee57e7f275e1ac914441f5676632
lizengcai <lizengcai_vip@sina.com>
2018-04-12 16:16:09 +0800

    mysql安装调整

--- php/linux.php
+++ php/linux.php
@@ -22,41 +22,90 @@ $arr = [
 /**
  *3.安装mysql
  */
-$arr1 = ['
-useradd -s /sbin/nologin mysql, //添加mysql用户,并且指定shell为/sbin/nologin
+$arr1 = <<<Install
+groupadd mysql
+useradd -g group -s /sbin/nologin -M mysql, //-s:定义shell,-M : 不建立根目录,-g:指定组
 mkdir -p /data/mysql/data,
-mkdir -p /usr/lcoal/mysql5.5,
 chown -R mysql:mysql /data/mysql,
 
 yum -y install gcc gcc-c++ cmake ncurses-devel bison,
-wget --no-check-certificate https://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.59.tar.gz, //下载
+wget --no-check-certificate https://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.39.tar.gz, //下载
 '];
+Install;
 
-$arr2 = [' //编译安装cmake+make+make install
-cmake -DMYSQL_USER=mysql -DCMAKE_INSTALL_PREFIX=/usr/local/mysql5.5 \
--DINSTALL_DATADIR=/data/mysql/data -DMYSQL_UNIX_ADDR=/tmp/mysqld.sock \
--DDEFAULT_CHARSET=utf8  -DDEFAULT_COLLATION=utf8_general_ci -DEXTRA_CHARSETS=all \
--DWITH_EMBEDDED_SERVER=1 -DENABLED_LOCAL_INFILE=1 -DWITH_MYISAM_STORAGE_ENGINE=1 -DWITH_INNOBASE_STORAGE_ENGINE=1,
+$arr2 = <<<Cmake
+//编译安装cmake+make+make install
+# cmake ./
+
+# -DCMAKE_INSTALL_PREFIX=/usr/local/mysql          \    #安装路径
+
+# -DMYSQL_DATADIR=/usr/local/mysql/data            \    #数据文件存放位置
+
+# -DSYSCONFDIR=/etc                                \    #my.cnf路径
+
+# -DWITH_MYISAM_STORAGE_ENGINE=1                   \    #支持MyIASM引擎
+
+# -DWITH_INNOBASE_STORAGE_ENGINE=1                 \    #支持InnoDB引擎
+
+# -DWITH_MEMORY_STORAGE_ENGINE=1                   \    #支持Memory引擎
+
+# -DWITH_READLINE=1                                \    #快捷键功能(我没用过)
+
+# -DMYSQL_UNIX_ADDR=/tmp/mysqld.sock               \    #连接数据库socket路径
+
+# -DMYSQL_TCP_PORT=3306                            \    #端口
+
+# -DENABLED_LOCAL_INFILE=1                         \    #允许从本地导入数据
+
+# -DWITH_PARTITION_STORAGE_ENGINE=1                \    #安装支持数据库分区
+
+# -DMYSQL_USER=mysql                                \   #mysqld运行用户
+
+# -DEXTRA_CHARSETS=all                             \    #安装所有的字符集
+
+# -DDEFAULT_CHARSET=utf8                           \    #默认字符
+
+# -DDEFAULT_COLLATION=utf8_general_ci
+# make && make install
+
+cmake ./ \
+-DCMAKE_INSTALL_PREFIX=/usr/local/mysql         \
+-DMYSQL_DATADIR=/data/mysql/data                \
+-DSYSCONFDIR=/etc                                \
+-DWITH_MYISAM_STORAGE_ENGINE=1                   \
+-DWITH_INNOBASE_STORAGE_ENGINE=1                 \
+-DMYSQL_UNIX_ADDR=/data/mysql/mysqld.sock        \
+-DMYSQL_TCP_PORT=3306                            \
+-DENABLED_LOCAL_INFILE=1                         \
+-DWITH_PARTITION_STORAGE_ENGINE=1                \
+-DMYSQL_USER=mysql                               \
+-DEXTRA_CHARSETS=all                             \
+-DDEFAULT_CHARSET=utf8                           \
+-DDEFAULT_COLLATION=utf8_general_ci
 
 make&&make install,
-chown -R mysql:mysql /usr/local/mysql5.5/,
-mv /root/mysql-5.5.59/support-files/my-large.cnf /etc/my.cnf //选择覆盖
-mv /root/mysql-5.5.59/support-files/mysql.server /etc/init.d/mysqld //服务端启动放入服务中service mysqld start
+
+chown -R mysql:mysql /usr/local/mysql/,
+mv /usr/local/src/mysql-5.6.39/support-files/my-default.cnf /etc/my.cnf //选择覆盖
+
+mv /usr/local/src/mysql-5.6.39/support-files/mysql.server /etc/init.d/mysqld //服务端启动放入服务中service mysqld start
 chmod a+x /etc/init.d/mysqld,
 chkconfig --level 345 mysqld on, //开机启动
 
-echo "export PATH=/usr/local/mysql5.5/bin/:$PATH" >> /etc/profile
+echo "export PATH=/usr/local/mysql/bin/:$PATH" >> /etc/profile
 source /etc/profile //设置环境变量,方便客户端启动
 
 //初始化设置(user,配置文件,基础目录,数据目录)
-/usr/local/mysql5.5/scripts/mysql_install_db \
+/usr/local/mysql/scripts/mysql_install_db \
 --user=mysql \
 --defaults-file=/etc/my.cnf \
---basedir=/usr/local/mysql5.5 \
+--basedir=/usr/local/mysql \
 --datadir=/data/mysql/data
 
 vim /etc/my.cnf
-basedir=/usr/local/mysql5.5
+user=mysql
+basedir=/usr/local/mysql
+default-storage-engine=Innodb
 datadir=/data/mysql/data //放置于mysql模块
 service mysqld reload
 service mysqld start
@@ -88,7 +137,7 @@ mysql -u root -p  -P port //登录
 -DWITH_FEDERATED_STORAGE_ENGINE=1 //安装frderated存储引擎
 -DWITH_PARTITION_STORAGE_ENGINE=1 //安装数据库分区
 -DINSTALL_PLUGINDIR=/usr/local/mysql/plugin //插件文件及配置路径
-'];
+Cmake;
 
 /**
  * 4.设置自动登录
--- php/mysql.php
+++ php/mysql.php
@@ -5,5 +5,5 @@
 $arr = [
     'mysqladmin -uroot -proot password + 新密码', //更改密码
     'mysql -uroot -p +数据库 < +sql路径',//导入sql文件
-    'mysqldump -uroot -p +数据库 > sql文件', //导出sql文件
+    'mysqldump -uroot -p [-d] +数据库 > sql文件', //导出sql文件,-d:no data(仅结构)
 ];
