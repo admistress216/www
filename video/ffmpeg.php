e95042622bc2c4306f3fab7f3ccc890a8d801271
lizengcai <lizengcai_vip@sina.com>
2018-04-28 11:09:11 +0800

    添加多视频合成

--- video/ffmpeg.php
+++ video/ffmpeg.php
@@ -5,6 +5,7 @@
 $arr = ['
 ffmpeg -i 1.mp4, //获取视频信息
 ffmpeg -i "concat:1524797315.mp3|1524797327.mp3" -acodec copy output.mp3, //连接语音
+-i 片头.wav -i 内容.WAV -i 片尾.wav -filter_complex \'[0:0] [1:0] [2:0] concat=n=3:v=0:a=1 [a]\' -map [a] 合成.wav
 '];
 
 /**
